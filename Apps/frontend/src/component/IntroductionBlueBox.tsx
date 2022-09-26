b69c3a8d2af33b485520c6b6bf2ec29a777c82e5
DHX98 <haox.dai@gmail.com>
2022-09-25 20:40:49 -0700

    - ADD bg

--- Apps/frontend/src/component/IntroductionBlueBox.tsx
+++ Apps/frontend/src/component/IntroductionBlueBox.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import BlueBoxBg from '../icons/blueboxbg.jpeg';
 import BlueBoxBgCellPhone from '../icons/BlueBoxBg_cellphone_size.png';
 
@@ -12,18 +13,41 @@ export default function IntroductionBlueBox() {
       component={Box}
       sx={{
         bgcolor: 'white',
-        width: '100%',
+        width: '100vw',
         height: '100vh',
       }}
-    >
-      <Box sx={{
-        display: {
-          xs: 'none', md: 'inline', lg: 'inline', xl: 'inline',
-        },
+      style={{
+        backgroundImage: `url(${BlueBoxBg})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
       }}
+    >
+      <Box
+        sx={{
+          display: {
+            xs: 'none', md: 'inline', lg: 'inline', xl: 'inline',
+          },
+
+        }}
+        justifyContent="center"
+
       >
-        <img src={BlueBoxBg} alt={BlueBoxBg} height="100%" width="100%" />
+        TODO:居中这里
+        <Typography
+          variant="h6"
+          sx={{
+            my: 2, flexGrow: 1, display: { xs: 'none', sm: 'block' },
+          }}
+        >
+          这里要居中
+        </Typography>
       </Box>
+
+      TODO:加一个md屏幕尺寸的bg
+
+      TODO:把下面的BOX改成和lg一样的
+
       <Box sx={{
         display: {
           xs: 'inline', md: 'none', lg: 'none', xl: 'none',

9f04e1eb598ae1fa25033cb1f7b31c3cd67cdf99
DHX98 <haox.dai@gmail.com>
2022-10-07 19:26:30 -0700

    -[BE] PW/Email not Null

--- Apps/backend/models/userModel.go
+++ Apps/backend/models/userModel.go
@@ -7,7 +7,7 @@ import (
 type User struct {
 	gorm.Model
 	UserName       string
-	PassWord       string `gorm:"size:255;not null"`
+	PassWord       string `gorm:"size:255;not null;default:null"`
 	HashedPassWord string `gorm:"size:255;not null"`
-	Email          string `gorm:"varchar(110);not null;unique"`
+	Email          string `gorm:"varchar(110);not null;unique;default:null"`
 }
